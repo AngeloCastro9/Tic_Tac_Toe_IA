4187c07077c0d491c190820186bce3de783c2c66
Ângelo Castro <42939130+AngeloCastro9@users.noreply.github.com>
2021-10-05 13:51:21 -0300

    clean code

--- TicTacToeIA/main.py
+++ TicTacToeIA/main.py
@@ -3,26 +3,23 @@ from TicTacToe import createBoard, makesMovement,  getValidInput, \
 
 from minimax import movementIA
 
-# se coloca 1 para o jogador inicar, 0 para a IA
 player = 0
 board = createBoard()
 winner = checkWinner(board)
 while(not winner):
     printBoard(board)
-    print("===================")
     if(player == 0):
         line, column = movementIA(board, player)
     else:
-        # maquina vs maquina
+        # machine vs machine
         # line, column = movementIA(board, player)
 
-        #jogador humano vs maquina
+        #human vs machine
         line = getValidInput("Enter the line: ")
         column = getValidInput("Enter the column: ")
     
     if(checkMovement(board, line, column)):
         makesMovement(board, line, column, player)
-        #troca o jogador, para que seja a vez do proximo jogador
         player = (player + 1)%2
     else:
         print("The informed position is already occupied")
--- TicTacToeIA/minimax.py
+++ TicTacToeIA/minimax.py
@@ -2,47 +2,29 @@ from TicTacToe import blankSpace, token, checkWinner
 
 def movementIA(board, player):
 
-    #pega todas as posicoes disponiveis, em branco
     possibilities = getPositions(board)
-    #melhor valor para se jogar
     best_value = None
-    #melhor movimento se da pensando em todas as possibilities e escolhendo o melhor movimento a se fazer
     best_movement = None
     
     for possibilitie in possibilities:
-        # se coloca o token do jogador em todos os campos possiveis
         board[possibilitie[0]][possibilitie[1]] = token[player]
-        # chama o algoritmo do minimax que irá retornar valores entre -1 e 1 para se fazer a jogada
         value = minimax(board, player)
 
-        # descomente para debug
-
-        # print(board)
-        # print(player)
-        
-        # fim debug
-
-        # se limpa todos os campos marcados para possibilidade
         board[possibilitie[0]][possibilitie[1]] = blankSpace
         if(best_value is None):
             best_value = value
             best_movement = possibilitie
-        #verifica se a IA e o jogador 0 ( o que a IA comeca )
         elif(player == 0):
             if(value > best_value):
                 best_value = value
-                best_movement = possibilitie
-        #verifica se a IA e o jogador 1 ( o que o jogador humano comeca )
+                best_movement = possibilitie        
         elif(player == 1):
             if(value < best_value):
                 best_value = value
                 best_movement = possibilitie
 
-    # a IA sempre irá mostrar 0%, até que o oponente cometa algum erro e ela irá ganhar com certeca
-    print("chance de vitória da IA: ", best_value*100, "%")
     return best_movement[0], best_movement[1]
 
-# pega todas as posicoes disponiveis para se jogar
 def getPositions(board):
     positions = []
     for line in range(3):
@@ -52,7 +34,6 @@ def getPositions(board):
     
     return positions
 
-# o minimax trabalha com valores de -1 a 1, cada um representa um token.
 score = {
     "DRAW": 0,
     "X": 1,
@@ -60,39 +41,24 @@ score = {
 }
 
 def minimax(board, player):
-    #verifica se existe um vencedor
     winner = checkWinner(board)
-    if(winner):
-        #retorna o ganhador/empate 
+    if(winner):        
         return score[winner]
-    #troca o jogador, para que seja a vez do proximo jogador
+
     player = (player + 1)%2 #%2 = resto da divisao por 2
-    #pega todas as posicoes 
     possibilities = getPositions(board)
-    #melhor valor para se jogar
     best_value = None
+
     for possibilitie in possibilities:
-        # se coloca o token do jogador em todos os campos possiveis
         board[possibilitie[0]][possibilitie[1]] = token[player]
-        # chama o algoritmo minimax
         value = minimax(board, player)
 
-        # descomente para debug
-
-        # print(board)
-        # print(player)
-
-        # fim debug
-
-        # se limpa todos os campos marcados para possibilidade
         board[possibilitie[0]][possibilitie[1]] = blankSpace
         if(best_value is None):
             best_value = value
-        #verifica se a IA e o jogador 0 ( o que a IA comeca )
         elif(player == 0):
             if(value > best_value):
                 best_value = value
-        #verifica se a IA e o jogador 1 ( o que o jogar humano comeca )
         elif(player == 1):
             if(value < best_value):
                 best_value = value
